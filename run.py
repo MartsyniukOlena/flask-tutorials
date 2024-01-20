83b609865e123c45d4ae55360619e1132782dcdf
MartsyniukOlena <>
2024-01-20 13:44:25 +0000

    Edit code

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 
@@ -27,14 +28,15 @@ def about_member(member_name):
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
-            if obj["name"] == member_name:
+            if obj["url"] == member_name:
                 member = obj
     return render_template("member.html", member=member)
 
+
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
-    if request.method == "POST"
-        flash("Thanks {}, we received your message!".format(
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
             request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
